297b82692768abd583a5f572fe394ea5dafa158a
Bobby Jones Jr <jones9682@yahoo.com>
2020-08-18 18:27:43 -0400

    added selectall, create, update and delete functions to interact with database

--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,37 @@
-// * Inside `burger.js`, import `orm.js` into `burger.js`
 
-// * Also inside `burger.js`, create the code that will call the ORM functions using burger specific input for the ORM.
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
 
-// * Export at the end of the `burger.js` file.
+var burger = {
+    //Select all burgers from database.
+    selectAll: function (cb) {
+        orm.all("burgers", function (res) {
+            cb(res);
+        });
+    },
+
+    //Create function to create/add a burger.
+    // The variables cols and vals are arrays.
+    create: function (cols, vals, cb) {
+        orm.create("burgers", cols, vals, function (res) {
+            cb(res);
+        });
+    },
+
+    //Update function to update burger devoured state.
+    update: function (objColVals, condition, cb) {
+        orm.update("burgers", objColVals, condition, function (res) {
+            cb(res);
+        });
+    },
+
+    //Delete function to throw away/delete burger from database.
+    delete: function (condition, cb) {
+        orm.delete("burgers", condition, function (res) {
+            cb(res);
+        });
+    }
+};
+
+// Export the database functions for the controller (burgers_controller.js).
+module.exports = burger;
