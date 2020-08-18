3a86dc315442562b0f2fefd0eec15a3fa73d6cff
Bobby Jones Jr <jones9682@yahoo.com>
2020-08-18 18:19:38 -0400

    added connection to localhost

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,20 @@
+// Node Dependency
+var mysql = require('mysql');
+var connection;
+
+// For Heroku Deployment vs. Local MySQL Database
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'Pa$$word', // Add your password
+        database: 'burgers_db' // Add your database
+    });
+}
 
 
-
-
-
-
-// module.exports()
+// Export the Connection
+module.exports = connection;
