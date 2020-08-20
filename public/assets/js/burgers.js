2d7f0bb5591769b271d506be13e3b9184b82f02f
Bobby Jones Jr <jones9682@yahoo.com>
2020-08-19 22:37:56 -0400

    added ajax calls and click/submit events

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,66 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function () {
+    //Click event for adding a burger.
+    $(".create-form").on("submit", function (event) {
+        // Make sure to preventDefault on a submit event.
+        event.preventDefault();
+        console.log($("#new_burger").val().trim());
+
+        //Grab burger name from form field.
+        //When user submits burger name, set devoured state to false.
+        var newBurger = {
+            burger_name: $("#new_burger").val().trim(),
+            devoured: 0
+        };
+
+        // Send the POST request using ajax.
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function () {
+                console.log("created new burger");
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+
+    //Click event to throw away/delete burger.
+    $(".delete-burger").on("click", function (event) {
+        var id = $(this).data("id");
+
+        // Send the DELETE request using ajax.
+        $.ajax("/api/burgers/" + id, {
+            type: "DELETE",
+        }).then(
+            function () {
+                console.log("deleted burger", id);
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+
+    //Click event for "Devour It" button.
+    $(".devour-button").on("click", function (event) {
+        var id = $(this).data("id");
+        var newDevour = $(this).data("devour");
+
+        var newDevourEd = {
+            devoured: newDevour
+        };
+
+        // Send the PUT request using ajax.
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newDevourEd
+        }).then(
+            function () {
+                console.log("changed devour to", newDevour);
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+});
